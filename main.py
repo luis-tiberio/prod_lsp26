b8495e7745af3b3ddb1f0581cce2f7527700caff
luis-tiberio <luisotaviotiberio@gmail.com>
2025-11-13 09:06:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ download_dir = "/tmp"
 # Cria o diretório, se não existir
 os.makedirs(download_dir, exist_ok=True)
 
+ops_id = os.environ.get('OPS_ID')
+ops_senha = os.environ.get('OPS_SENHA')
+
 def login(page):
     page.goto("https://spx.shopee.com.br/")
     page.wait_for_selector('xpath=//*[@placeholder="Ops ID"]', timeout=15000)
-    page.fill('xpath=//*[@placeholder="Ops ID"]', 'Ops34139')
-    page.fill('xpath=//*[@placeholder="Senha"]', '@Shopee1234')
+    page.locator('xpath=//*[@placeholder="Ops ID"]').fill(ops_id)
+    page.locator('xpath=//*[@placeholder="Senha"]').fill(ops_senha)
     page.click('xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button')
 
     page.wait_for_timeout(15000)
@@ -32,33 +35,25 @@ def get_data(page):
     data = []
     try:
         # Primeiro link
-        page.goto("https://spx.shopee.com.br/#/dashboard/facility-soc/historical-data")
+        page.goto("https://spx.shopee.com.br/#/dashboard/toProductivity?page_type=Inbound")
         #page.wait_for_selector('xpath=//*[@id="mgmt-dashboard-content"]/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/canvas', timeout=45000)
         page.wait_for_timeout(10000)
-        first_value = page.inner_text('xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[25]')
+        first_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div')
         data.append(first_value)
 
         # Segundo link
         page.goto("https://spx.shopee.com.br/#/dashboard/toProductivity?page_type=Outbound")
         page.wait_for_timeout(10000)
-        page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div', timeout=30000)
+        # page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div', timeout=30000)
         second_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div')
         data.append(second_value)
 
         # Terceiro dado
         page.click('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[3]')
         page.wait_for_timeout(10000)
-        page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div', timeout=30000)
+        # page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div', timeout=30000)
         third_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div')
         data.append(third_value)
-
-        # Quarto dado
-        page.click('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[4]')
-        page.wait_for_timeout(10000)
-        page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div', timeout=30000)
-        page.wait_for_timeout(20000)
-        fourth_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/table/thead/tr[2]/th[4]/div/div')
-        data.append(fourth_value)
 
     except Exception as e:
         print(f"Erro ao coletar dados: {e}")
@@ -69,7 +64,7 @@ def update_google_sheets(data):
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("hxh.json", scope)
     client = gspread.authorize(creds)
-    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1R1Ywt_8SuT3X154l1dS30NJEP-JZgoWzH-oq4bOJld0/edit?gid=0#gid=0').worksheet("Python")
+    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1wj7LJM_RFwf1ZMOIPAAwG2Ax8yAHYCQA6gN-ITlKv3Q').worksheet("Dados prod")
 
     current_time = datetime.datetime.now(timezone)
     if 7 <= current_time.hour <= 23:
@@ -83,7 +78,7 @@ def update_google_sheets(data):
         return
 
     if 2 <= row_number <= 25:
-        cell_range = f'B{row_number}:F{row_number}'
+        cell_range = f'B{row_number}:D{row_number}'
         values = [data]
         sheet.update(cell_range, values)
         print(f"Dados atualizados na linha {row_number} ({cell_range})")
